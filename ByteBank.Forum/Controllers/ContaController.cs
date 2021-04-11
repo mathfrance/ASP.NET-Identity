7d313ae8ee80ca2c314152dff0e56177ed628031
Matheus <mathfrance@hotmail.com>
2021-04-11 06:57:51 -0300

    Parte 1 - Aula 1

---
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,23 @@
+﻿using ByteBank.Forum.ViewsModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
